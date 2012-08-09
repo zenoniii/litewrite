c453342edad76be5501888ca0ccc6ae0c232ab2c
Jorin Vogel <jorin.vogel@gmail.com>
2012-08-09 18:59:23 +0200

    clean up some event binding

--- js/litewrite.js
+++ js/litewrite.js
@@ -5,33 +5,14 @@ define(function(require) {
   var cache = require('utils/cache');
   var settings = require('models/settings');
 
-  function setOpenDoc() {
-    cache.openDoc = docs.get( settings.get('openDocId') );
-  }
-
-  function setWindowTitle() {
-    document.title = 'Litewrite: ' + cache.openDoc.get('title');
-  }
 
   function litewrite() {
 
     setOpenDoc();
 
-    settings.on('change:openDocId', function() {
-      setOpenDoc();
-      setWindowTitle();
-
-      var previousDoc = docs.get(settings.previous('openDocId'));
-      //When doc gets cloesd remove spaces and returns at the beginning
-      //and remove spaces at the end of each line
-      var previousContent = previousDoc.get('content').replace(/^<.*?>([^<].*?)(<\/div>|(?=<br>)|$)/, '$1');
-      //Contenteditable never is really empty
-      if ( !_.isNull(previousContent.match(/^(<\/{0,1}div>|<br>|\s|&nbsp;)*?$/)) || _.isEmpty(previousContent) ) {
-        previousDoc.destroy();
-      } else {
-        previousDoc.save('content', previousContent);
-      }
-    });
+    settings.on('change:openDocId', setOpenDoc);
+    settings.on('change:openDocId', setWindowTitle);
+    settings.on('change:openDocId', deleteEmpty);
 
     docs.on('change:title', setWindowTitle);
     setWindowTitle();
@@ -46,6 +27,28 @@ define(function(require) {
       new AppView();
     });
 
+  }
+
+
+  function setOpenDoc() {
+    cache.openDoc = docs.get( settings.get('openDocId') );
+  }
+
+  function setWindowTitle() {
+    document.title = 'Litewrite: ' + cache.openDoc.get('title');
+  }
+
+  function deleteEmpty() {
+    var previousDoc = docs.get(settings.previous('openDocId'));
+    //When doc gets cloesd remove spaces and returns at the beginning
+    //and remove spaces at the end of each line
+    var previousContent = previousDoc.get('content').replace(/^<.*?>([^<].*?)(<\/div>|(?=<br>)|$)/, '$1');
+    //Contenteditable never is really empty
+    if ( !_.isNull(previousContent.match(/^(<\/{0,1}div>|<br>|\s|&nbsp;)*?$/)) || _.isEmpty(previousContent) ) {
+      previousDoc.destroy();
+    } else {
+      previousDoc.save('content', previousContent);
+    }
   }
 
 
