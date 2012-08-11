9e1908de6938949631fd0b8271b744b9d9de67ed
Jorin Vogel <jorin.vogel@gmail.com>
2012-08-11 16:46:47 +0200

    added description for model.toggle()

--- js/utils/customizations.js
+++ js/utils/customizations.js
@@ -3,6 +3,7 @@ define(function(require) {
   var Backbone = require('backbone');
 
 
+  //toggle a boolean attribute of the model
   Backbone.Model.prototype.toggle = function(attr, options) {
     var data = {};
     data[attr] = !this.get(attr);
