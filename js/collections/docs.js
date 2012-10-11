1ac9bdb7f4b0d16b9f0e327e8a159f8ca1677b55
Jorin Vogel <jorin.vogel@gmail.com>
2012-10-11 21:22:37 +0200

    improved url generation

--- js/collections/docs.js
+++ js/collections/docs.js
@@ -12,6 +12,12 @@ define(function(require) {
 
   remoteStorage.claimAccess('documents', 'rw');
 
+
+  var escapeRegExp = function(str) {
+    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+  };
+
+
   var Docs = Backbone.Collection.extend({
 
     model: Doc,
@@ -66,7 +72,7 @@ define(function(require) {
         return;
       }
       var len = this.filter(function(doc) {
-	return new RegExp('^' + url + '(-[0-9]|$)').test(doc.get('url'));
+        return new RegExp('^' + escapeRegExp(url) + '(-[0-9]|$)').test(doc.get('url'));
       }).length;
       url = len < 1 ? url : url + '-' + len;
       doc.set('url', url);
