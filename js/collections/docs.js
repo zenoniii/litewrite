edb10488df774a34ee42ad1c5d521cd69f17e739
Jorin Vogel <jorin.vogel@gmail.com>
2013-05-05 14:40:04 +0200

    use a nice escapeRegExp function

--- js/collections/docs.js
+++ js/collections/docs.js
@@ -81,8 +81,11 @@ define(function(require) {
   });
 
 
+  // see link for more info:
+  // http://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript/3561711#3561711
   function escapeRegExp(str) {
-    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+
   }
 
 
