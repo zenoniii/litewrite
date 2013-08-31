7d02982a58675b12fe781148f7eb07aafda8039d
Jorin Vogel <jorin.vogel@gmail.com>
2013-09-01 00:13:15 +0200

    move welcome.md to welcome.html

--- src/collections/docs.js
+++ src/collections/docs.js
@@ -8,7 +8,7 @@ define(function(require) {
 
   var remoteStorageDocuments = require('remotestorage-documents');
   var rsSync = require('rs-adapter');
-  var welcome = require('text!templates/welcome.md');
+  var welcome = require('text!templates/welcome.html');
 
 
 
