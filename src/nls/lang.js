053d8eeaa4d54bef30decbd01eba081b6bb58d15
Jorin Vogel <jorin.vogel@gmail.com>
2014-02-17 20:52:38 +0100

    forgot to add files for i18n -.- (#192)

---
+++ src/nls/lang.js
@@ -0,0 +1,52 @@
+define(function(require) {
+
+  // set language for momentjs
+  var moment = require('moment');
+  moment.lang((navigator.language || navigator.browserLanguage).slice(0, 2));
+
+
+  return {
+
+    root: {
+        emptyDoc: 'Write …',
+        search: 'Search …',
+        footer: 'write lite, open source',
+        share: 'share',
+        open: 'open',
+        modified: 'modified',
+        welcome: require('text!templates/welcome.txt')
+    },
+
+    fr: {
+        emptyDoc: 'Écrire …',
+        search: 'Search …',
+        footer: 'write lite, open source',
+        share: 'Partagez',
+        open: 'open',
+        modified: 'modified',
+        welcome: require('text!templates/welcome-fr.txt')
+    },
+
+    ru: {
+        emptyDoc: 'Пишите …',
+        search: 'Search …',
+        footer: 'пишите с легкостью. код открыт',
+        share: 'Поделиться',
+        open: 'open',
+        modified: 'modified',
+        welcome: require('text!templates/welcome-ru.txt')
+    },
+
+    de: {
+        emptyDoc: 'Schreibe etwas …',
+        search: 'Suchen …',
+        footer: 'write lite, open source',
+        share: 'teilen',
+        open: 'öffnen',
+        modified: 'zuletzt bearbeitet',
+        welcome: require('text!templates/welcome-de.txt')
+    }
+
+  };
+
+});
