b26950103c5fb3d97ac95925ac48931e01e1c593
Max Golubev <3demax@ukr.net>
2015-11-10 16:07:10 +0200

    Removed redundant propetries

--- src/translations/ru.js
+++ src/translations/ru.js
@@ -11,31 +11,19 @@ module.exports = {
   open:     'открыть',
   modified: 'изменен',
   welcome:  require('./welcome-ru.txt'),
-  secondAgo:   function ()  { return 'секунду назад'     ; },
-  secondsAgo:  function (x) { return x + ' seconds ago' ; },
-  minuteAgo:   function ()  { return 'минуту назад'     ; },
-  minutesAgo:  function (x) { return x + ' minutes ago' ; },
-  hourAgo:     function ()  { return 'час назад'      ; },
-  hoursAgo:    function (x) { return x + ' hours ago'   ; },
-  dayAgo:      function ()  { return 'день назад'        ; },
-  daysAgo:     function (x) { return x + ' days ago'    ; },
-  monthAgo:    function ()  { return 'месяц назад'      ; },
-  monthsAgo:   function (x) { return x + ' months ago'  ; },
-  yearAgo:     function ()  { return 'год назад'       ; },
-  yearsAgo:    function (x) { return x + ' years ago'   ; }
   timeAgo: function relativeTimeWithPlural(number, key) {
-        var format = {
-            'mm': 'минуту_минуты_минут',
-            'hh': 'час_часа_часов',
-            'dd': 'день_дня_дней',
-            'MM': 'месяц_месяца_месяцев',
-            'yy': 'год_года_лет'
-        };
-        if (key === 'm') {
-            return 'минуту';
-        }
-        else {
-            return number + ' ' + plural(format[key], +number);
-        }
-    }
+      var format = {
+          'mm': 'минуту_минуты_минут',
+          'hh': 'час_часа_часов',
+          'dd': 'день_дня_дней',
+          'MM': 'месяц_месяца_месяцев',
+          'yy': 'год_года_лет'
+      };
+      if (key === 'm') {
+          return 'минуту';
+      }
+      else {
+          return number + ' ' + plural(format[key], +number);
+      }
+  }
 };
