ec465d532bb6381bec2b167a6ea92fc7584ed9e0
Jorin Vogel <hi@jorin.me>
2015-11-17 19:50:18 +0700

    Update autosize trigger event. Should fix this https://github.com/litewrite/litewrite/pull/225#issuecomment-155076156. Also closes #232 for now.

--- src/views/editor.js
+++ src/views/editor.js
@@ -30,7 +30,9 @@ var EditorView = Backbone.View.extend({
     // this is just for migration from contenteditable to textarea.
     // we can remove this later on.
     content = content.replace(/<br>/ig,'\n').replace(/<\/br>/ig,'\n').replace(/<div>/ig,'\n').replace(/<[^>]+>/ig,'').replace(/\&amp;/ig,'&');
-    this.$el.val(content || '').trigger('autosize.resize');
+    this.$el
+      .val(content || '')
+      .trigger('autosize:update');
   },
 
   focus: function() {
