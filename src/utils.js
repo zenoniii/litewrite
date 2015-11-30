41be997aa1cf3c60b4f926f09513e05453215b5d
Jorin Vogel <hi@jorin.me>
2015-11-30 14:45:56 +0700

    Prompt user to reload on cache change This way the user can get the latest version of Litewrite right away.
    I figured, we don't need i18n for this ...

--- src/utils.js
+++ src/utils.js
@@ -54,4 +54,15 @@ utils.timeAgo = function (date) {
   return translations[quantifier.name](count)
 }
 
+utils.handleAppcacheUpdates = function () {
+  var appcache = window.applicationCache
+  if (!appcache) return
+  appcache.addEventListener('updateready', function (e) {
+    if (appcache.status !== appcache.UPDATEREADY) return
+    var msg = 'A new version of Litewrite is available. Load it?'
+    if (!window.confirm(msg)) return
+    window.location.reload()
+  }, false)
+}
+
 module.exports = utils
