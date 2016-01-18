b07c8dcd1c0df5f5545f63ae43f5f83ca13698b2
Jorin Vogel <hi@jorin.me>
2016-01-18 15:48:16 +0700

    Only trigger connected if rs says so. Fixes #257. I found the remoteStorage.connected property and use this now to check if remoteStorage is actually connected.

--- src/litewrite.js
+++ src/litewrite.js
@@ -81,7 +81,7 @@ _.extend(Litewrite.prototype, Backbone.Events, {
   },
 
   triggerConnected: function () {
-    this.trigger('connected')
+    if (remoteStorage.connected) this.trigger('connected')
   },
 
   triggerDisconnected: function () {
