eab509bb9d9632f3c66264a75aa11f8184e01e0d
Jorin Vogel <hi@jorin.me>
2017-08-04 15:58:13 +0200

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,5 @@ echo "$DEPLOY_KEY" > deploy_key.pem
 chmod 600 deploy_key.pem # this key should have push access
 ssh-add deploy_key.pem
 git remote add deploy $REPO_URI
+git checkout .
 git push deploy master
